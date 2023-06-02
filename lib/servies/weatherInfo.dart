e8410211f37eb305b9f2b7a14ff9d461afd3a5d2
jjonghyn <jjonghyunn0802@gmail.com>
2023-06-02 11:54:52 +0900

    apiURL작성

--- lib/servies/weatherInfo.dart
+++ lib/servies/weatherInfo.dart
@@ -1,5 +1,15 @@
 
-//기상청api
-const apiKey = '';
+//기상청 api 초단기 실황 조회
+const getUltraSrtNcstURL = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst';
+//기상청 api 초단기 예보 조회
+const getUltraSrtFcstURL = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst';
+//기상청 api 단기 예보 조회
+const getVilageFcstURL = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+//기상청 api 중기 기온 조회
+const getMidTaURL = 'https://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa';
+//기상청 api 중기 육상 예보 조회
+const getMidLandFcstURL = 'https://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst';
 
-// const
+//공공데이터에서 발급받은 나의 기상청 api key
+const apiKey = '9OX4Slh4W3dxrLci5bFKmza/QEZJp8tfW9cna8a6T7GjZEwX1RSQPO/wXg4vemOPpQIh6S23eDDgT0SDDfTiRw==';
+
